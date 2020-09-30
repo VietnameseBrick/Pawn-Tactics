348b5b26ca0c2c61510a43bc25b48960d675284c
VietnameseBrick <nanguyen@ucsd.edu>
2020-09-29 18:13:15 -0700

    Sprint and collision bug fix

---
+++ Assets/Sprint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprint : MonoBehaviour
+{
+    PlayerMovement basicMovementScript;
+    public float speedBoost = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basicMovementScript = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            basicMovementScript.moveSpeed += speedBoost;
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            basicMovementScript.moveSpeed -= speedBoost;
+        
+    }
+}
